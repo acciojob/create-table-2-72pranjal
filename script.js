94d73753c286add24c63fdf236251f52b4160046
Pranjal Gupta <109664273+72pranjal@users.noreply.github.com>
2024-12-17 09:38:22 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,19 @@
+let table = document.querySelector('#myTable')
 function createTable() {
     //Write your code here
+	 let rows = prompt("Input number of rows", "");
+                let columns = prompt("Input number of columns", '');
+                if(rows !== null && columns !== null) {
+                    for(let i=0; i<+rows; i++) {
+                        let tableRow = document.createElement('tr')
+                        for(let j=0; j<+columns; j++) {
+                            let tableData = document.createElement('td')
+                            tableData.innerText = `Row-${i} Column-${j}`
+                            tableRow.append(tableData)
+                        }
+                        table.append(tableRow)
+                    }
+                }
+	
   
 }
